6a9170b0c28cbd5fa6bc2ea678d07eff7bd2b779
snRNA <sn201411123015@gmail.com>
2016-07-22 16:46:51 +0800

    simplify codes
    simplify codes

---
+++ Files/Webhome/upload/listfiles.php
@@ -0,0 +1,78 @@
+﻿<html>
+<head>
+    <meta charset='UTF-8'>
+    <title>
+        文件中继站
+    </title>
+    <style>
+    </style>
+</head>
+<center>
+
+    <?php
+    session_start();
+
+    include $_SERVER['DOCUMENT_ROOT'] . "/libs/includes.php";
+    include $Connect_Lib;
+
+    function GetFile_Encrypted($SavedFileName)
+    {
+        return file_get_contents($upload_folder . $SavedFileName);
+    }
+
+    $result = mysql_query("SELECT * FROM file");
+
+    echo "
+        <div id='main-table-div'>
+            <table border='1' style='table-layout: fixed;  text-overflow:ellipsis;text-align: center;'>
+                <thead style='overflow-y: auto;display: block'>
+                    <tr id='#main-table-th-tb'>
+                        <th class='col1'>编号</th>
+                        <th class='col2'>文件名(点击下载加密的文件)</th>
+                        <th class='col3'>文件大小</th>
+                        <th class='col4'>上传者</th>
+                        <th class='col5'>MD5</th>
+                        <th class='col6'>SHA-1</th>
+                        <th class='col7'>上传时间</th>
+                        <th class='col8'>文件校验</th>
+                    </tr>
+                </thead>
+         ";
+    echo "<tbody style='height: 300px;overflow-y: auto;display: block'>";
+    while ($row = mysql_fetch_array($result)) {
+        $saveFilename = $row['savefilename'];
+        $filename = $row['filename'];
+        $filename_encode = urlencode($filename);
+        $fileID = $row['fid'];
+        $MD5 = $row['MD5'];
+        $SHA1 = $row['SHA1'];
+        echo "<tr id='#main-table-th-tb'>";
+        echo "<td class='col1'>" . $row['fid'] . "</td>";
+        echo "<td class='col2' style='overflow: hidden;text-overflow: ellipsis;white-space: nowrap;'>
+                <a href=download.php?file=$saveFilename&saveas=$filename_encode
+                title='$filename'>$filename</a>
+              </td>";
+        echo "<td class='col3'>" . round($row['size'] / 1024) . "KB</td>";
+        echo "<td class='col4'>" . $row['ownername'] . "</td>";
+        echo "<td class='col5'>
+                <a href='/echo.php?echo=$MD5'  target='_blank' title='$MD5' >显示</a>
+                <a href='/echo.php?echo=$MD5&download=1&save_as={$filename}_md5.txt'  target='_blank' title='$MD5' >下载</a>
+              </td>";
+        echo "<td class='col6'>
+                <a href='/echo.php?echo=$SHA1' target='_blank' title='$SHA1'>显示</a>
+                <a href='/echo.php?echo=$SHA1&download=1&save_as={$filename}_sha1.txt' target='_blank' title='$SHA1'>下载</a>
+              </td>";
+        echo "<td class='col7'>" . $row['uploadtime'] . "</td>";
+        echo "<td class='col8'><a href=put_check_file.php?fid=$fileID>校验</a></td>";
+        echo "</tr>";
+    }
+    echo "</tbody>";
+
+    echo "</table>
+        </div>";
+
+    mysql_close($conn);
+    ?>
+
+</center>
+</html>
---
+++ Files/Webhome/upload/upload_do.php
@@ -0,0 +1,89 @@
+﻿<?php
+include $_SERVER['DOCUMENT_ROOT'] . "/not_login_jump.php";
+
+session_start();
+include $_SERVER['DOCUMENT_ROOT'] . "/libs/includes.php";
+include $Connect_Lib;
+include $_SERVER['DOCUMENT_ROOT'] . "/libs/filecheck.php";
+
+$fileName = $_FILES["file"]["name"];
+$fileType = $_FILES["file"]["type"];
+$fileSize = $_FILES["file"]["size"];
+$tempFile = $_FILES["file"]["tmp_name"];
+
+if (!preg_match('/^[\x80-\xff\w\-\_\.\#\+]+$/', $fileName)) {
+    exit('文件名不能包含特殊字符！<a href="javascript:history.back(-1);">返回</a>');
+}
+
+if ($fileSize > 10 * 1024 * 1024) {
+    echo "上传文件大小超过10MB!";
+    exit();
+}
+
+if (checkFiletype($fileType)) {
+    if ($_FILES["file"]["error"] > 0) {
+        exit("Return Code: " . $_FILES["file"]["error"]);
+    } else {
+
+        //文件名处理
+        $file_hash_md5 = md5_file($tempFile);
+        $file_hash_sha1 = sha1_file($tempFile);
+
+        if (file_exists($upload_folder . $file_hash_sha1)) {
+            //debug -- 省存储空间
+            $upload__result = "Same file already exists.";
+        } else {
+
+            //加密文件
+            $outputfilepath = $upload_folder . $file_hash_sha1;
+            $outputfilename = $file_hash_sha1;
+            $upload_iv = fencrypt($tempFile, $outputfilepath);
+            //move_uploaded_file($_FILES["file"]["tmp_name"],$upload_folder . $file_hash_sha1);
+            unlink($_FILES["file"]["tmp_name"]);
+
+            //显示文件信息
+            echo "文件名: " . $fileName . "<br />";
+            echo "文件类型: " . $fileType . "<br />";
+            echo "文件大小: " . ($fileSize / 1024) . " Kb<br />";
+            //echo "Temp file: " . $tempFile . "<br />";
+            //echo "文件路径: " . $outputfilepath. "<br />";
+
+            $upload__username = $_SESSION['username'];
+            $upload__userid = $_SESSION['userid'];
+			
+			$result=mysql_query("select prikey from user where uid='$upload__userid' limit 1");
+            $row=mysql_fetch_array($result);
+            $priKey=$row['prikey'];
+            $enfile_hash_sha1 = sha1_file($outputfilepath);
+            $enfile_hash_md5 = md5_file($outputfilepath);
+            openssl_sign($enfile_hash_sha1, $signaturesha1, $priKey, OPENSSL_ALGO_SHA256);
+            openssl_sign($enfile_hash_md5, $signaturemd5, $priKey, OPENSSL_ALGO_SHA256);
+			$signaturesha1=base64_encode($signaturesha1);
+			$signaturemd5=base64_encode($signaturemd5);
+			
+            $sql = "INSERT INTO file(filename,size,savefilename,ownername,ownerid,uploadtime,MD5,SHA1,iv,directory,MD5S,SHA1S)
+                                    VALUES('$fileName',
+                                            '$fileSize',
+                                            '$outputfilename',
+                                            '$upload__username',
+                                            '$upload__userid',
+                                            CURRENT_TIME(),
+                                            '$file_hash_md5',
+                                            '$file_hash_sha1',
+                                            '$upload_iv',
+											'$outputfilepath',
+											'$signaturemd5',
+											'$signaturesha1')";
+            if (mysql_query($sql, $conn)) {
+                $upload__result = "success";
+            } else {
+                $upload__result = "fail";
+                //删除临时文件
+                unlink($outputfilepath);
+            }
+        }
+    }
+} else {
+    $upload__result="Type Error";
+}
+?>
